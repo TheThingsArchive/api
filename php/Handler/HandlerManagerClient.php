287c428746f038590303f3e3d4905bb777b18f6d
Roman Volosatovs <rvolosatovs@riseup.net>
2017-07-27 11:26:04 +0200

    [protos,mocks] regenerate

--- php/Handler/HandlerManagerClient.php
+++ php/Handler/HandlerManagerClient.php
@@ -5,34 +5,30 @@
 // Copyright © 2017 The Things Network
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 //
-namespace Handler;
+namespace Handler {
 
-/**
- * The HandlerManager service provides configuration and monitoring
- * functionality
- */
-class HandlerManagerClient extends \Grpc\BaseStub {
+  // The HandlerManager service provides configuration and monitoring
+  // functionality
+  class HandlerManagerClient extends \Grpc\BaseStub {
 
-    /**
-     * @param string $hostname hostname
-     * @param array $opts channel options
-     * @param \Grpc\Channel $channel (optional) re-use channel object
-     */
+     // @param string $hostname hostname
+     // @param array $opts channel options
+     // @param \Grpc\Channel $channel (optional) re-use channel object
     public function __construct($hostname, $opts, $channel = null) {
-        parent::__construct($hostname, $opts, $channel);
+      parent::__construct($hostname, $opts, $channel);
     }
 
-    /**
-     * @param \Handler\StatusRequest $argument input argument
-     * @param array $metadata metadata
-     * @param array $options call options
-     */
+     // @param \Handler\StatusRequest $argument input argument
+     // @param array $metadata metadata
+     // @param array $options call options
     public function GetStatus(\Handler\StatusRequest $argument,
       $metadata = [], $options = []) {
-        return $this->_simpleRequest('/handler.HandlerManager/GetStatus',
-        $argument,
-        ['\Handler\Status', 'decode'],
-        $metadata, $options);
+      return $this->_simpleRequest('/handler.HandlerManager/GetStatus',
+      $argument,
+      ['\Handler\Status', 'decode'],
+      $metadata, $options);
     }
+
+  }
 
 }
--- php/Networkserver/NetworkServerManagerClient.php
+++ php/Networkserver/NetworkServerManagerClient.php
@@ -5,34 +5,30 @@
 // Copyright © 2017 The Things Network
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 //
-namespace Networkserver;
+namespace Networkserver {
 
-/**
- * The NetworkServerManager service provides configuration and monitoring
- * functionality
- */
-class NetworkServerManagerClient extends \Grpc\BaseStub {
+  // The NetworkServerManager service provides configuration and monitoring
+  // functionality
+  class NetworkServerManagerClient extends \Grpc\BaseStub {
 
-    /**
-     * @param string $hostname hostname
-     * @param array $opts channel options
-     * @param \Grpc\Channel $channel (optional) re-use channel object
-     */
+     // @param string $hostname hostname
+     // @param array $opts channel options
+     // @param \Grpc\Channel $channel (optional) re-use channel object
     public function __construct($hostname, $opts, $channel = null) {
-        parent::__construct($hostname, $opts, $channel);
+      parent::__construct($hostname, $opts, $channel);
     }
 
-    /**
-     * @param \Networkserver\StatusRequest $argument input argument
-     * @param array $metadata metadata
-     * @param array $options call options
-     */
+     // @param \Networkserver\StatusRequest $argument input argument
+     // @param array $metadata metadata
+     // @param array $options call options
     public function GetStatus(\Networkserver\StatusRequest $argument,
       $metadata = [], $options = []) {
-        return $this->_simpleRequest('/networkserver.NetworkServerManager/GetStatus',
-        $argument,
-        ['\Networkserver\Status', 'decode'],
-        $metadata, $options);
+      return $this->_simpleRequest('/networkserver.NetworkServerManager/GetStatus',
+      $argument,
+      ['\Networkserver\Status', 'decode'],
+      $metadata, $options);
     }
+
+  }
 
 }
